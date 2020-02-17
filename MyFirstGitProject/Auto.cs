45fe2b215a7dfed83e8e31c7adf19ad907ad391e
Kékesi Bence <bence.kekesi2@gmail.com>
2020-02-17 09:37:11 +0100

    Félkész beolvasással

---
+++ MyFirstGitProject/Auto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitProject
+{
+    class Auto
+    {
+        private string marka;
+        private string modell;
+        private int évjárat;
+        private int kilometer;
+        private string uzemanyag;
+        private int ar;
+
+        public Auto(string marka, string modell, int évjárat, int kilometer, string uzemanyag, int ar)
+        {
+            this.marka = marka;
+            this.modell = modell;
+            this.évjárat = évjárat;
+            this.kilometer = kilometer;
+            this.uzemanyag = uzemanyag;
+            this.ar = ar;
+        }
+
+        public string Marka { get => marka; set => marka = value; }
+        public string Modell { get => modell; set => modell = value; }
+        public int Évjárat { get => évjárat; set => évjárat = value; }
+        public int Kilometer { get => kilometer; set => kilometer = value; }
+        public string Uzemanyag { get => uzemanyag; set => uzemanyag = value; }
+        public int Ar { get => ar; set => ar = value; }
+
+        public override string ToString()
+        {
+            return String.Format("Márka: {0}, Modell: {1}, Évjárat: {2}, Kilométer: {3}, Uzemanyag: {4}, Ár: {5}", Marka, Modell, Évjárat, Kilometer, Uzemanyag, Ar);
+        }
+    }
+}
--- MyFirstGitProject/Program.cs
+++ MyFirstGitProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MyFirstGitProject
 {
@@ -10,7 +11,27 @@ namespace MyFirstGitProject
     {
         static void Main(string[] args)
         {
+            string sor; 
             Console.WriteLine("Kacsa");
+            StreamReader olvas = new StreamReader("auto.csv");
+            List<Auto> autos = new List<Auto>(); 
+            olvas.ReadLine();
+            do
+            {
+                sor = olvas.ReadLine();
+                string[] i = sor.Split(';');
+                Auto a = new Auto(i[0], i[1], Convert.ToInt32(i[2]), Convert.ToInt32(i[3]), i[4], Convert.ToInt32(i[5]));
+                autos.Add(a);
+
+
+            } while (!olvas.EndOfStream);
+
+            Console.WriteLine(autos[1]);
+            olvas.Close();
+            Console.ReadKey();
+
         }
+
+
     }
 }
